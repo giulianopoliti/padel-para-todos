0f8e9f044b18ed06850a630bc3650167c075b897
Giuliano <giulianopoliti77@gmail.com>
2025-07-08 19:38:48 -0300

    change tournaments

--- app/(main)/tournaments/tournaments-client.tsx
+++ app/(main)/tournaments/tournaments-client.tsx
@@ -370,7 +370,7 @@ function TournamentCard({
         <div className="flex items-center text-sm text-gray-600">
           <Users className="h-4 w-4 mr-2 text-gray-400 flex-shrink-0" />
           <span>
-            {tournament.currentParticipants || 0}/{tournament.maxParticipants || "∞"} parejas
+            {tournament.currentParticipants || 0}/{tournament.maxParticipants || "∞"} participantes
           </span>
         </div>
 
